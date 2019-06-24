3c9ea2970ef941e59554df01c34e5700dea6434c
Phonekham <phonekham.dev@gmail.com>
2019-06-24 20:08:34 +0700

    9. The Context Provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -16,13 +17,19 @@ class App extends React.Component {
           <i
             className="flag us"
             onClick={() => this.onLangguageChange("english")}
-          />
+          >
+            E
+          </i>
           <i
             className="flag nl"
             onClick={() => this.onLangguageChange("dutch")}
-          />
+          >
+            D
+          </i>
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
